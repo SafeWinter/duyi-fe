fd12d8df5e41b45d6d6b28475964feadc06329cb
SafeWinter <zandong_19@aliyun.com>
2025-05-17 23:58:36 +0800

    added notes for 13_Third_library:L05: Mock.js introduction

---
+++ 13_3rd_libs/L05_MockJS/exercises/after/mock.js
@@ -0,0 +1,13 @@
+Mock.mock('/api/cart', 'get', {
+  code: 0,
+  msg: '',
+  'data|5-20': [
+    {
+      productName: '@csentence',
+      productUrl: '@image(180x150, #008c8c, #fff, test image)',
+      'unitPrice|1-500.2': 0,
+      'count|1-10': 0,
+    },
+  ],
+});
+
