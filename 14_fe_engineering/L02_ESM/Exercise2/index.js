a8c193ac559fc3ceae14140a6c19299391e13b92
SafeWinter <zandong_19@aliyun.com>
2025-05-21 16:51:05 +0800

    updated notes for 14_fe_engineering:L01: CommonJS module system

---
+++ 14_fe_engineering/L02_ESM/Exercise2/index.js
@@ -0,0 +1,11 @@
+// import test from './test.js';
+// console.log(test);
+
+// import test, { a, b } from './test.js';
+// console.log(test, a, b);
+
+// import * as test from './test.js';
+// console.log(test);
+
+import './test.js';
+
