b4ec67e71b272ef59062636fee150e50f5a53866
SafeWinter <zandong_19@aliyun.com>
2025-05-27 23:01:48 +0800

    added notes for 14_Vue_intro:L05: setup for demo Vue project my-site

---
+++ 15_Vue_intro/proj1_my_site/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>App 组件</h1>
+    <Avatar :size="size" :url="url" />
+  </div>
+</template>
+
+<script>
+import Avatar from './components/Avatar';
+
+export default {
+  name: 'App',
+  components: {
+    Avatar
+  },
+  data() {
+    return {
+      url: 'http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c',
+      size: 150
+    }
+  },
+}
+</script>
---
+++ 15_Vue_intro/proj1_my_site/src/components/Avatar.vue
@@ -0,0 +1,32 @@
+<template>
+  <img 
+    class="avatar-img"
+    :src="url" 
+    :style="{
+        width: `${size}px`, 
+        height: `${size}px`
+    }">
+</template>
+
+<script>
+export default {
+    props: {
+        url: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+            default: 50
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.avatar-img {
+    border-radius: 50%;
+    object-fit: cover;
+    display: block;
+}
+</style>
