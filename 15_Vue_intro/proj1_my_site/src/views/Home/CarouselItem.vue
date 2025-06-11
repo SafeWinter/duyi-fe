01b8b883999dc6afa51201801bb2d773d7a8e3cf
SafeWinter <zandong_19@aliyun.com>
2025-06-11 14:49:23 +0800

    added notes for 14_Vue_intro:L15: Main page implementation (Part 1/3)

---
+++ 15_Vue_intro/proj1_my_site/src/views/Home/CarouselItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="carousel-item-container">
+    <slot name="default"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CarouselItem',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      // You can add any local state here if needed
+    };
+  },
+  methods: {
+    // Define any methods you need for this component
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.carousel-item-container {
+  width: 100%;
+  height: 100vh;
+}
+li {
+  list-style: none;
+}
+</style>
--- 15_Vue_intro/proj1_my_site/src/views/Home/index.vue
+++ 15_Vue_intro/proj1_my_site/src/views/Home/index.vue
@@ -1,60 +1,32 @@
 <template>
   <div class="home-container" ref="home">
-    <h1 ref="h1">{{ count }}</h1>
-    <button @click="count++">count++</button>
-    <ul>
-      <li v-for="b in banners" :key="b.id">
-        <img :src="b.midImg" alt="Banner Image" />
-        <p>{{ b.title }}</p>
-        <p>{{ b.description }}</p>
-      </li>
-    </ul>
+    <CarouselList :data="banners"/>
   </div>
 </template>
 
 <script>
 import { getBanner } from '@/api/banner';
+import CarouselList from './CarouselList.vue';
 
 export default {
+  components: {
+    CarouselList
+  },
   data() {
     return {
-      count: 0,
       banners: [],
     };
   },
-  // 测试【注入】前后
-  beforeCreate() {
-    console.log('beforeCreate', this.count);  // beforeCreate undefined
-  },
   async created() {
-    console.log('created', this.count);  // created 0
     this.banners = await getBanner();
-    // console.log(this.banners);
-  },
-  // 测试【生成真实 DOM】前后
-  beforeMount() {
-    console.log('beforeMount', this.$refs.home);  // beforeMount undefined
-  },
-  mounted() {
-    console.log('mounted', this.$refs.home); // mounted DOM
-  },
-  // 测试【更新】前后
-  beforeUpdate() {
-    const count = this.$refs.h1.innerText;
-    console.log('beforeUpdate', count, this.count); // beforeUpdate 0 1
-  },
-  updated() {
-    const count = this.$refs.h1.innerText;
-    console.log('updated', count, this.count);  // updated 1 1
-  },
-  // 测试【销毁】前后
-  beforeDestroy() {
-    console.log('beforeDestroy', this.$refs.home); // beforeDestroy DOM
-  },
-  destroyed() {
-    console.log('destroyed', this.$refs.home); // destroyed undefined
   },
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
